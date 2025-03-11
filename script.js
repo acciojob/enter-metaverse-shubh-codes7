fd82f5f4e13bd1dc45cc48aa60d7c54b7cd6132b
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-11 17:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let heading = document.getElementById("status")
+document.getElementById("enterBtn").addEventListener("click", ()=> {
+	heading.innerHTML = "<h1>Entered Metaverse</h1>"
+})
